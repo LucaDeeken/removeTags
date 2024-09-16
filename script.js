e83525664277349d9513553f1016fc80ee8653d0
LucaDeeken <168202686+LucaDeeken@users.noreply.github.com>
2024-09-16 16:02:19 +0200

    add function for removing HtmlTags

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const wholeHtmlCode = document.body.outerHTML;
+const stripHTMLTags = str => str.replace(/<[^>]*>/g, '');
+const getPlainText = stripHTMLTags(wholeHtmlCode);
+    
+console.log(getPlainText);
